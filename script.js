c13dd71a05353b4f3fd453f592d67c8ffdfb6c0e
jP <silva.joao@icen.ufpa.br>
2024-11-27 22:49:21 -0300

    feat: criando a função printnames

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const addBtn = document.querySelector("#addNamesBtn");
 const nameBar = document.querySelector("#barToInsrNames");
 const divNames = document.querySelector("#names")
+const trashBtn = document.querySelector(".fa-solid .fa-trash")
 const namesList = []
+
+
 addBtn.addEventListener("click", () => {
-    addBtn.style.color  = "red"
-    divNames.innerHTML += `<p> ${nameBar.value}</p>`
+    divNames.innerHTML += printnames(nameBar.value)
     namesList.push(nameBar.value)
-    nameBar.textContent = ""
+    nameBar.value = ""
 });
 console.log(namesList)
 
+trashBtn.addEventListener("click", () => {
+    addBtn.remove()
+});
+
 function printnames(name){
-    
+    const html = `<section class="nameAndTrash">
+        <p>${name}</p>
+        <i class="fa-solid fa-trash"></i>
+    </section>
+    <hr>`
+    return html
 }
